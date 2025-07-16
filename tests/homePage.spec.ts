5b98b8a882669773cc6fb358c74023ce4bcad5f8
bgabice <benjamingabice@gmail.com>
2025-07-16 11:44:18 +0200

    wrote a smoke test to verify elements exists on the homepage

---
+++ tests/homePage.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await expect(page.getByText('Welcome back')).toBeVisible();
+  await expect(page.getByText('Enter your email and password to sign in')).toBeVisible();
+  await expect(page.getByText('Enter ID or Passport or Cell Phone Number')).toBeVisible();
+  await expect(page.getByPlaceholder('Password')).toBeVisible();
+  await expect(page.getByText('Remember Me')).toBeVisible();
+  await expect(page.getByText('Forgot password?')).toBeVisible();
+  await expect(page.getByText('New application?')).toBeVisible();
+  await expect(page.locator('form')).toContainText('Click here');
+  await expect(page.getByText('HillCross College Student Management System')).toBeVisible();
+});
